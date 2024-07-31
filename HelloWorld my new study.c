8e91f8eeb4c9bd8b93776fe64c1cef6970a5de70
XieFField <3432674139@qq.com>
2024-07-31 13:46:18 +0800

    Add files via upload

---
+++ HelloWorld my new study.c
@@ -0,0 +1,12 @@
+#include<stdio.h>//头文件，head
+//代码从main 函数开始执行
+/*此为单行注释*/
+//快捷键crtl+F5,执行程序，编译程序
+int main()
+{
+	printf("HelloWorld!\n\n\n\n\n");//用于格式化输出屏幕
+	/*print为打印，f为函数*/
+	printf("我嘞个豆\n");
+	return 0;//语句用于表示退出程序
+
+}
